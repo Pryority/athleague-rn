b299280ecd92ee62c3bf0a638651fcb5a11bdcc8
pryority <matthewapryor@gmail.com>
2024-02-08 16:21:16 -0500

    🐛 Dragging marker updaates description w/ coord

--- Map.tsx
+++ Map.tsx
@@ -1,5 +1,10 @@
-import React, { Dispatch, useRef, useState } from "react";
-import { MapOverlay } from "react-native-maps";
+import React, { Dispatch, useEffect, useRef, useState } from "react";
+import {
+  MapOverlay,
+  MapPressEvent,
+  MarkerDragEvent,
+  MarkerDragStartEndEvent,
+} from "react-native-maps";
 
 import { type MapMarker } from "./types";
 import {
@@ -23,7 +28,7 @@ const MapScreen = ({
 }) => {
   const mapRef = useRef<any>();
   const [canAddMarkers, setCanAddMarkers] = useState(true);
-  const [currentCpIndex, setCurrentCpIndex] = useState(0);
+  const [currentCpIndex, setCurrentCpIndex] = useState<null | number>(null);
   const [deletedCheckpoints, setDeletedCheckpoints] = useState<string[]>([]);
   const dropAreaOverlay = useRef<MapOverlay>();
 
@@ -42,14 +47,14 @@ const MapScreen = ({
     return distance > threshold;
   };
 
-  const handleMapPress = (event: any) => {
-    if (!canAddMarkers) return;
+  const handleMapPress = (event: MapPressEvent) => {
+    if (!canAddMarkers || !event.nativeEvent.coordinate) return;
     const newCoordinate = event.nativeEvent.coordinate;
     const newMarker = {
       id: markers.length + 1,
       coordinate: newCoordinate,
-      title: `Checkpoint ${markers.length + 1}`,
-      description: "",
+      title: `Checkpoint: ${markers.length + 1}`,
+      description: `Lat: ${newCoordinate.latitude} | Lng: ${newCoordinate.longitude}`,
     };
     const { latitude, longitude } = event.nativeEvent.coordinate;
     console.log(`==> MAP PRESS - lat: ${latitude}, lng: ${longitude}`);
@@ -77,6 +82,54 @@ const MapScreen = ({
     );
   };
 
+  const handleMarkerDragEnd = (e: MarkerDragStartEndEvent) => {
+    // Extracting coordinate and id from the native event
+    const { coordinate, id } = e.nativeEvent;
+
+    // Checking if id is not undefined
+    if (id !== undefined) {
+      // Mapping through the markers array
+      const updatedMarkers = markers.map((marker) =>
+        // Updating the marker with the matching id
+        marker.id === Number(id)
+          ? {
+              ...marker,
+              description: `Lat: ${coordinate.latitude} | Lng: ${coordinate.longitude}`,
+              coordinate,
+            }
+          : marker
+      );
+      // Setting the state with the updated markers, explicitly casting to MapMarker[]
+      setMarkers(updatedMarkers);
+      console.log("Updated Markers (Drag End):", updatedMarkers);
+    }
+  };
+
+  const handleMarkerDrag = (e: MarkerDragEvent, draggedMarker: MapMarker) => {
+    // Extracting coordinate and id from the native event
+    const { coordinate, id } = e.nativeEvent;
+
+    // Checking if id is not undefined
+    if (id !== undefined) {
+      // Mapping through the markers array
+      const updatedMarkers = markers.map((marker) =>
+        // Updating the dragged marker with the matching id
+        marker.id === draggedMarker.id
+          ? {
+              ...marker,
+              description: `Lat: ${coordinate.latitude} | Lng: ${coordinate.longitude}`,
+              coordinate,
+            }
+          : marker
+      );
+
+      setMarkers(updatedMarkers);
+      console.log("Dragging Marker: ", draggedMarker.id);
+      console.log(e.nativeEvent.coordinate);
+      console.log("Updated Markers (Dragging):", updatedMarkers);
+    }
+  };
+
   const handleDeleteMarker = (markerId: number) => {
     setMarkers((prevMarkers: MapMarker[]) =>
       prevMarkers.filter((marker) => marker.id !== markerId)
@@ -84,9 +137,14 @@ const MapScreen = ({
   };
 
   const handleMarkerClick = (marker: MapMarker) => {
-    console.log(`Clicked Marker: ${marker.id}`);
+    setCurrentCpIndex(marker.id - 1);
     mapRef.current?.animateToRegion(marker.coordinate);
+    console.log(`Clicked Marker: ${marker.id}`);
   };
+
+  useEffect(() => {
+    console.log("currentCpIndex", currentCpIndex);
+  }, [currentCpIndex]);
 
   return (
     <StyledMapView
@@ -96,8 +154,8 @@ const MapScreen = ({
       initialRegion={{
         latitude: 37.83,
         longitude: -122.51777381728886,
-        latitudeDelta: 0.02,
-        longitudeDelta: 0.02,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
       }}
       onPress={(map) => {
         handleMapPress(map);
@@ -115,14 +173,18 @@ const MapScreen = ({
             console.log(`Marker ${marker.id} callout pressed`)
           }
           onPress={() => handleMarkerClick(marker)}
+          onDragEnd={(e) => handleMarkerDragEnd(e)}
+          onDrag={(e) => {
+            handleMarkerDrag(e, marker);
+          }}
         >
           <StyledView
             className={`${
               index === 0
-                ? "bg-lime-500"
+                ? "bg-lime-500/90"
                 : index === markers.length - 1
-                ? "bg-yellow-500"
-                : "bg-cyan-100"
+                ? "bg-yellow-500/90"
+                : "bg-cyan-100/90"
             } p-4 rounded-full border-2 border-black relative justify-center items-center`}
           >
             <StyledText className="absolute">{marker.id}</StyledText>
